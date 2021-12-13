bb90ed1419a593a863909431c86c2233be7e5c87
Daniel Khromov <danielkhromov@gmail.com>
2021-12-13 23:23:10 +0300

    remove 2 page on prod

--- menu.py
+++ menu.py
@@ -4,13 +4,14 @@ from graphs import graph_page
 from predict import prediction_page
 import os
 
-if not os.path.isdir('Streamlit-Image-Carousel/frontend/public'):
-    print('INSTALLING NPM MODULES')
-    os.system('cd Streamlit-Image-Carousel/frontend/')
-    os.system('cd Streamlit-Image-Carousel/frontend/; npm i; npm run build; cd ../../')
+# trying to install npm in streamlit mmmmmmm
+# if not os.path.isdir('Streamlit-Image-Carousel/frontend/public'):
+#     print('INSTALLING NPM MODULES')
+#     os.system('cd Streamlit-Image-Carousel/frontend/')
+#     os.system('cd Streamlit-Image-Carousel/frontend/; npm i; npm run build; cd ../../')
 
 st.set_page_config(layout="wide")
-pages = {'Интерактивная карта': interactive_map, 'Факторы, влияющие на цену': graph_page,
+pages = {'Интерактивная карта': interactive_map, #'Факторы, влияющие на цену': graph_page,
          'Предсказание цены': prediction_page}
 st.sidebar.title('Меню')
 choice = st.sidebar.radio("Выберите страницу:", tuple(pages.keys()))
