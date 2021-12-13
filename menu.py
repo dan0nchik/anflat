c87a38e56c69e1a318c14db324f09fe01981edbe
Daniel Khromov <danielkhromov@gmail.com>
2021-12-13 23:16:21 +0300

    npm install + rename dano

--- menu.py
+++ menu.py
@@ -2,6 +2,12 @@ import streamlit as st
 from map_page import interactive_map
 from graphs import graph_page
 from predict import prediction_page
+import os
+
+if not os.path.isdir('Streamlit-Image-Carousel/frontend/public'):
+    print('INSTALLING NPM MODULES')
+    os.system('cd Streamlit-Image-Carousel/frontend/')
+    os.system('cd Streamlit-Image-Carousel/frontend/; npm i; npm run build; cd ../../')
 
 st.set_page_config(layout="wide")
 pages = {'Интерактивная карта': interactive_map, 'Факторы, влияющие на цену': graph_page,
