cc4e34d40436ea2311f55c2dfbc2f70a934b6924
ChaikaChaikaChaika <61208152+ChaikaChaikaChaika@users.noreply.github.com>
2021-12-16 00:03:24 +0300

    metro added to the map

--- map_page.py
+++ map_page.py
@@ -10,7 +10,7 @@ def interactive_map():
     callback = ("function (row) {"
                 "var icon = L.AwesomeMarkers.icon({ icon: 'icon',markerColor: 'red'});"
                 "var marker = L.marker([row[0], row[1]], {icon: icon}); "
-                "var popup = L.popup({maxWidth: '400'});"
+                "var popup = L.popup({maxWidth: '300'});"
                 "const display_text = {text: row[2]};"
                 "var mytext = $(`<div id='mytext' class='display_text' "
                 "style='width: 100.0%; height: 100.0%;'> ${display_text.text}</div>`)[0];"
@@ -18,7 +18,7 @@ def interactive_map():
                 "marker.bindPopup(popup);"
                 "return marker};")
 
-    dff = pd.read_excel('adressesV3.xlsx')
+    dff = pd.read_excel('adresses.xlsx')
     # print(dff)
     location = [55.7522, 37.61200]
     zoom_start = 10
@@ -27,13 +27,14 @@ def interactive_map():
     for i in range(len(dff)):
         # df = pd.DataFrame(arr, columns=['lat', 'lon', 'link', 'string'])
         r = f"<br><b>Адрес:</b> {dff.iloc[i, 1]}.<br>"
-        r = r + f"<br><b>Район:</b> {dff.iloc[i, 4]}<br>"
-        r = r + f"<br><b>Проект:</b> {dff.iloc[i, 5]}<br>"
-        r = r + f"<br><b>Девелопер:</b> {dff.iloc[i, 6]}<br>"
-        r = r + f"<br><b>Класс:</b> {dff.iloc[i, 7]}<br>"
-        r = r + f"<br><b>Минимальная цена за кв.м:</b> {dff.iloc[i, 8]}<br>"
-        r = r + f"<br><b>Средняя цена за кв.м:</b> {dff.iloc[i, 9]}<br>"
-        r = r + f"<br><b>Максимальная цена за кв.м:</b> {dff.iloc[i, 10]}<br>"
+        r = r + f"<br><b>Район:</b> {dff.iloc[i, 8]}<br>"
+        r = r + f"<br><b>Проект:</b> {dff.iloc[i, 9]}<br>"
+        r = r + f"<br><b>Девелопер:</b> {dff.iloc[i, 10]}<br>"
+        r = r + f"<br><b>Класс:</b> {dff.iloc[i, 11]}<br>"
+        r = r + f"<br><b>Минимальная цена за кв.м:</b> {dff.iloc[i, 12]}<br>"
+        r = r + f"<br><b>Средняя цена за кв.м:</b> {dff.iloc[i, 13]}<br>"
+        r = r + f"<br><b>Максимальная цена за кв.м:</b> {dff.iloc[i, 14]}<br>"
+        r = r + f"<br><b>Ближайшая станция метро :</b> {dff.iloc[i, 4]} на расстоянии <b>{round(float(dff.iloc[i, 7]), 1)} метров</b><br>"
         s.append(r)
 
     dff['текст'] = s
